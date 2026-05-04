4ed95851f432ac0594c37dd89d848491497ce509
Ktols <genskynyrd@gmail.com>
2026-05-03 21:35:44 -0500

    chore: setup capacitor for android apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.easypod.app',
+  appName: 'EasyPod',
+  webDir: 'dist'
+};
+
+export default config;
